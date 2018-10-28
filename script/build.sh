1630aee203bb11e0ef4e3e73a54d4eb743f7daa6
hz <838223935@qq.com>
2018-10-28 15:33:28 +0800

    mod scrpt

--- script/build.sh
+++ script/build.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 
 set -e
-#SHELL = /bin/bash
-#BASEDIR = $(shell pwd)
-
-# env variables
-# you should set these follow variables before exec `make build`
-# GOPATH: go path
-# PROJ_DIR: project path
 PROJ_DIR=github.com/hzxiao
 PROJ_NAME=taskmeter
-#BRANCH: project git branch
 BRANCH=master
-
 PROJ_GIT_REPO=https://github.com/hzxiao/taskmeter.git
 PROJ_FULL_DIR=$GOPATH/src/$PROJ_DIR
 PROJ_FULL_NAME=$PROJ_FULL_DIR/$PROJ_NAME
@@ -39,19 +30,23 @@ if [ ! -d ${PROJ_FULL_NAME} ]; then
 	git clone $PROJ_GIT_REPO
 fi
 
-	cd $PROJ_FULL_NAME
-	git checkout .
-	git checkout $BRANCH
-	git pull
+cd $PROJ_FULL_NAME
+git checkout .
+git checkout $BRANCH
+git pull
 
-	rm -rf $OUTPUT_DIR
-	mkdir -p $OUTPUT_DIR/$PROJ_NAME
-	mkdir -p $OUTPUT_DIR/$PROJ_NAME/conf
-	mkdir -p $OUTPUT_DIR/$PROJ_NAME/script
-	go build -v -ldflags "-w -X ${versionDir}.gitTag=${gitTag} -X ${versionDir}.buildDate=${buildDate} -X ${versionDir}.gitCommit=${gitCommit} -X ${versionDir}.gitTreeState=${gitTreeState}" .
-	cp -R conf/* $OUTPUT_DIR/$PROJ_NAME/conf
-	cp -R script/* $OUTPUT_DIR/$PROJ_NAME/script
-	mv $PROJ_NAME $OUTPUT_DIR/$PROJ_NAME
-	cp Makefile $OUTPUT_DIR/$PROJ_NAME
-	cd $OUTPUT_DIR
-	zip -r -q $PROJ_NAME.zip $PROJ_NAME
+rm -rf $OUTPUT_DIR
+mkdir -p $OUTPUT_DIR/$PROJ_NAME
+mkdir -p $OUTPUT_DIR/$PROJ_NAME/conf
+mkdir -p $OUTPUT_DIR/$PROJ_NAME/script
+go build -v -ldflags "-w -X ${versionDir}.gitTag=${gitTag} -X ${versionDir}.buildDate=${buildDate} -X ${versionDir}.gitCommit=${gitCommit} -X ${versionDir}.gitTreeState=${gitTreeState}" .
+cp -R conf/* $OUTPUT_DIR/$PROJ_NAME/conf
+cp -R script/* $OUTPUT_DIR/$PROJ_NAME/script
+mv $PROJ_NAME $OUTPUT_DIR/$PROJ_NAME
+cp Makefile $OUTPUT_DIR/$PROJ_NAME
+cd $OUTPUT_DIR
+zip -r -q $PROJ_NAME.zip $PROJ_NAME
+
+if [ -d ${SAVE_PKG_DIR} ]; then
+    cp $PROJ_NAME.zip ${SAVE_PKG_DIR}
+fi
