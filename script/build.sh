757d4a830db219a585f7d8cf29a671aa38795af4
hz <838223935@qq.com>
2018-11-03 16:38:30 +0800

    mod build script:  swag

--- script/build.sh
+++ script/build.sh
@@ -35,13 +35,18 @@ git checkout .
 git checkout $BRANCH
 git pull
 
+swag init
+
 rm -rf $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR/$PROJ_NAME
 mkdir -p $OUTPUT_DIR/$PROJ_NAME/conf
 mkdir -p $OUTPUT_DIR/$PROJ_NAME/script
+mkdir -p $OUTPUT_DIR/$PROJ_NAME/docs
+
 go build -v -ldflags "-w -X ${versionDir}.gitTag=${gitTag} -X ${versionDir}.buildDate=${buildDate} -X ${versionDir}.gitCommit=${gitCommit} -X ${versionDir}.gitTreeState=${gitTreeState}" .
 cp -R conf/* $OUTPUT_DIR/$PROJ_NAME/conf
 cp -R script/* $OUTPUT_DIR/$PROJ_NAME/script
+cp -R docs/* $OUTPUT_DIR/$PROJ_NAME/docs
 mv $PROJ_NAME $OUTPUT_DIR/$PROJ_NAME
 cp Makefile $OUTPUT_DIR/$PROJ_NAME
 cd $OUTPUT_DIR
