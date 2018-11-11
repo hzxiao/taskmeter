d754903b18f89badf9ed8d09ff04a7083b47e0dc
hz <838223935@qq.com>
2018-11-11 22:44:36 +0800

    fix cors

--- router/router.go
+++ router/router.go
@@ -3,13 +3,14 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/hzxiao/taskmeter/handler"
+	"github.com/hzxiao/taskmeter/router/middleware"
 	"net/http"
 )
 
 func Load(g *gin.Engine, mw ...gin.HandlerFunc) *gin.Engine {
 	g.Use(gin.Recovery())
-	//g.Use(middleware.NoCache)
-	//g.Use(middleware.Options)
+	g.Use(middleware.NoCache)
+	g.Use(middleware.Options)
 	g.Use(mw...)
 	// 404 Handler.
 	g.NoRoute(func(c *gin.Context) {
